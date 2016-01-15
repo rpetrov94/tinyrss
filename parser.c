34d2e4ff8c1f37745d163c88c35efdea73aaf150
Robert Petrov <rpetrov94@outlook.com>
2016-01-15 16:15:19 +0000

    refactored parser

--- parser.c
+++ parser.c
@@ -31,20 +31,31 @@ RSS* parse_string(char* string)
   return rss;
 }
 
+typedef void (*SETTER_FUNC) (void*, const char*);
+void cleanup_after_set(SETTER_FUNC fn, void* item, char* string)
+{
+  (*fn)(item, string);
+  xmlFree(string);
+}
+
 void build_rss_item(RSSItem* item, xmlNodePtr node)
 {
-  if (strcmp((char*) node->name, "title") == 0) 
-    rss_item_set_title(item, CLEAN(xmlNodeGetContent(node)));
+  SETTER_FUNC setter_func = NULL;
+  if (strcmp((char*) node->name, "title") == 0)
+    setter_func = (SETTER_FUNC) rss_item_set_title;
   else if (strcmp((char*) node->name, "link") == 0) 
-    rss_item_set_link(item, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_item_set_link;
   else if (strcmp((char*) node->name, "description") == 0) 
-    rss_item_set_description(item, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_item_set_description;
   else if (strcmp((char*) node->name, "author") == 0) 
-    rss_item_set_author(item, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_item_set_author;
   else if (strcmp((char*) node->name, "guid") == 0) 
-    rss_item_set_guid(item, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_item_set_guid;
   else if (strcmp((char*) node->name, "pub_date") == 0) 
-    rss_item_set_pub_date(item, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_item_set_pub_date;
+
+  if (setter_func != NULL)
+    cleanup_after_set(setter_func, item, CLEAN(xmlNodeGetContent(node)));
 
   if (node->next == NULL) return;
   build_rss_item(item, node->next);
@@ -52,24 +63,28 @@ void build_rss_item(RSSItem* item, xmlNodePtr node)
 
 void build_rss(RSS* rss, xmlNodePtr node)
 {
+  SETTER_FUNC setter_func = NULL;
   if (strcmp((char*) node->name, "title") == 0) 
-    rss_set_title(rss, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_set_title;
   else if (strcmp((char*) node->name, "link") == 0) 
-    rss_set_link(rss, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_set_link;
   else if (strcmp((char*) node->name, "description") == 0) 
-    rss_set_description(rss, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_set_description;
   else if (strcmp((char*) node->name, "language") == 0) 
-    rss_set_language(rss, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_set_language;
   else if (strcmp((char*) node->name, "copyright") == 0) 
-    rss_set_copyright(rss, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_set_copyright;
   else if (strcmp((char*) node->name, "pub_date") == 0) 
-    rss_set_pub_date(rss, CLEAN(xmlNodeGetContent(node)));
+    setter_func = (SETTER_FUNC) rss_set_pub_date;
   else if (strcmp((char*) node->name, "item") == 0)
   {
     RSSItem* item = create_rss_item();
     build_rss_item(item, node->children);
     rss_add_rss_item(rss, item);
   }
+
+  if (setter_func != NULL)
+    cleanup_after_set(setter_func, rss, CLEAN(xmlNodeGetContent(node)));
 
   if (node->next == NULL) return;
   build_rss(rss, node->next);
