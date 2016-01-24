d6fa1bf4ae39cafe884a402ad8a521533a589eca
Robert Petrov <rpetrov94@outlook.com>
2016-01-24 13:48:23 +0000

    removed old setup

--- helper.h
+++
@@ -1,8 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-void set_string(char**, const char*);
-void pretty_print_node(char*, char*, char*);
-char* trim(char *);
-
-#endif
--- parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "rss.h"
-
-RSS* parse_string(char*);
-
-#endif
